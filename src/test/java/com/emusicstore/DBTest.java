9e79e1a7217f247c6ccbc6b1395f6fd18bb17f20
vchiriac <valentin_chiriac@yahoo.com>
2017-07-21 11:43:00 +0200

    Initial commit

---
+++ src/test/java/com/emusicstore/DBTest.java
@@ -0,0 +1,33 @@
+package com.emusicstore;
+
+import com.emusicstore.configuration.HibernateConfiguration;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration()
+@ContextConfiguration(classes = {HibernateConfiguration.class}, loader = AnnotationConfigWebContextLoader.class)
+public class DBTest {
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+
+
+    @Test
+    public void testDummy() throws SQLException {
+        String schema = dataSource.getConnection().getCatalog();
+        assertTrue("emusicstore".equalsIgnoreCase(schema));
+    }
+
+}
